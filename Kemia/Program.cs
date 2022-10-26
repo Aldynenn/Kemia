b298222fd0341dc324efd1a505f87d311ef5614c
Aldynenn <marcell.lazar2003@gmail.com>
2022-10-26 14:54:59 +0200

    Ötödik feladat kész

--- Kemia/Program.cs
+++ Kemia/Program.cs
@@ -16,13 +16,49 @@ namespace Kemia
             AdatokBeolvasasa();
             HarmadikFeladat();
             NegyedikFeladat();
+            string vegyjel = "";
+            OtodikFeladat(out vegyjel);
+            HatodikFeladat(vegyjel);
 
             Console.ReadKey();
         }
 
+        private static void HatodikFeladat(string vegyjel)
+        {
+            bool megvan = false;
+            int i = 0;
+            Kemia keresett;
+            while (!megvan && i < adatok.Count)
+            {
+                if (adatok[i].Vegyjel.ToLower() == vegyjel.ToLower())
+                {
+                    megvan = true;
+                    keresett = adatok[i];
+                }
+                i++;
+            }
+
+            if (megvan)
+            {
+                Console.WriteLine("\t");
+            }
+        }
+
+        private static void OtodikFeladat(out string vegyjel)
+        {
+            vegyjel = "";
+            while (!(vegyjel.Length <= 2 && vegyjel.Length >= 1 && vegyjel.All(Char.IsLetter)))
+            {
+                Console.Write("5. feladat: Kérek egy vegyjelet: ");
+                vegyjel = Console.ReadLine();
+            }
+        }
+
         private static void NegyedikFeladat()
         {
-
+            int okor = 0;
+            foreach (var adat in adatok) if (adat.Ev.ToLower() == "ókor") okor++;
+            Console.WriteLine($"4. feladat: Felfedezések száma az ókorban: {okor}");
         }
 
         private static void HarmadikFeladat()
