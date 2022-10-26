ce90ca336d0da20cbbf83fbbf4dfa2dc37adb8b5
c18lázárm <c18lzrm@201-11>
2022-10-26 14:35:20 +0200

    Add project files.

---
+++ Kemia/Kemia.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemia
+{
+    class Kemia
+    {
+        private string ev;
+
+        public string Ev
+        {
+            get { return ev; }
+            set { ev = value; }
+        }
+
+        private string elem;
+
+        public string Elem
+        {
+            get { return elem; }
+            set { elem = value; }
+        }
+
+        private string vegyjel;
+
+        public string Vegyjel
+        {
+            get { return vegyjel; }
+            set { vegyjel = value; }
+        }
+
+        private int rendszam;
+
+        public int Rendszam
+        {
+            get { return rendszam; }
+            set { rendszam = value; }
+        }
+
+        private string felfedezo;
+
+        public string Felfedezo
+        {
+            get { return felfedezo; }
+            set { felfedezo = value; }
+        }
+
+        public Kemia(string row)
+        {
+            List<string> temp = row.Split(';').ToList();
+            string ev = temp[0];
+            string elem = temp[1];
+            string vegyjel = temp[2];
+            int rendszam = Convert.ToInt32(temp[3]);
+            string felfedezo = temp[4];
+            Ev = ev;
+            Elem = elem;
+            Vegyjel = vegyjel;
+            Rendszam = rendszam;
+            Felfedezo = felfedezo;
+        }
+
+        public override string ToString()
+        {
+            return $"| {Ev} | {Elem} | {Vegyjel} | {Rendszam} | {Felfedezo} |";
+        }
+    }
+}
---
+++ Kemia/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Kemia
+{
+    class Program
+    {
+        static List<Kemia> adatok = new List<Kemia>();
+
+        static void Main(string[] args)
+        {
+            AdatokBeolvasasa();
+            HarmadikFeladat();
+            NegyedikFeladat();
+
+            Console.ReadKey();
+        }
+
+        private static void NegyedikFeladat()
+        {
+
+        }
+
+        private static void HarmadikFeladat()
+        {
+            Console.WriteLine($"3. feladat: Elemek száma: {adatok.Count}");
+        }
+
+        private static void AdatokBeolvasasa()
+        {
+            using (StreamReader sr = new StreamReader("felfedezesek.csv"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    adatok.Add(new Kemia(sr.ReadLine()));
+                }
+            }
+
+            /*
+            foreach (var a in adatok)
+            {
+                Console.WriteLine(a.ToString());
+            }
+            */
+        }
+    }
+}
